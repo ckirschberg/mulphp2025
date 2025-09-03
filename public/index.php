0275b8fc3cf6e151162c7ed9b4efbd88316ed56d
Christian Kirschberg <ckirschberg@gmail.com>
2025-09-03 11:22:45 +0200

    php opret

--- public/index.php
+++ public/index.php
@@ -16,19 +16,56 @@ $request = $_SERVER['REQUEST_METHOD'];
 
 // Create an if statement to check if this is a GET request
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=mul2025", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn = new PDO("mysql:host=$servername;dbname=mul2025", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $statement = $conn->query("SELECT * FROM cats");
-  $result = $statement->fetchAll();
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-  echo json_encode($result);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+// echo $uri;
+
+// $uri = explode('/', $uri);
+
+
+if ($request === 'GET' && $uri === '/pips') {
+    try {
+        $statement = $conn->query("SELECT * FROM cats");
+        $result = $statement->fetchAll();
+
+        echo json_encode($result);
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+} 
+else if ($request === 'POST' && $uri === '/pips') {
+    $input = (array) json_decode(file_get_contents('php://input'), true);
+
+    $name = $input["name"];
+    $color = $input["color"];
+
+    if ($name !== '') { // validering: overholde regler for at gemme korrekt data
+        $data = [
+            'name' => $name,
+            'color' => $color
+        ];
+        $sql = "INSERT INTO cats VALUES (default, :name, :color)";
+        $stmt= $conn->prepare($sql);
+        $stmt->execute($data);
+
+
+        $id = $conn->lastInsertId();
+        $cat = (object) $input;
+        $cat->id = $id;
+
+        echo json_encode($cat);
+    } else {
+        echo json_encode("Navn skal udfyldes");
+    }
+
+    // echo $name;
+    
+
 }
-
 // else check if this is a POST request and write "You wrote a POST request" back
 
 
